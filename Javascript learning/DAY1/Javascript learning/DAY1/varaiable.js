05dc9bd1dff13321a3e0bd8aa61d0009ce9d8352
Ayishwarya C <124068540+ayishwaryaC@users.noreply.github.com>
2025-10-07 22:27:07 +0530

    Add files via upload

---
+++ Javascript learning/DAY1/Javascript learning/DAY1/varaiable.js
@@ -0,0 +1,31 @@
+// Variables and Constants
+//     - `var`, `let`, and `const` (difference and scope)
+//     - Hoisting concept
+
+
+// there are three way to declare the varaible var let and const
+// var a = 10;
+// let b = 20;
+// const c = 22.7;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+
+// Using variable before declaring
+// console.log(e);
+// var e = 22;
+
+
+// console.log(b); 
+// let b = 20;
+
+
+// Function hoisting
+
+sayhello();
+function sayhello(){
+    console.log("Hello from Ayishu");
+}
